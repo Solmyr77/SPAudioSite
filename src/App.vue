6f1a8580b89ba7c97c2b4d0a921777d7b9fec104
Solmyr77 <91564294+Solmyr77@users.noreply.github.com>
2024-03-05 09:45:02 +0100

    V1.0

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap');
+
+body {
+  margin: 0px;
+}
+
+.dm-sans-bold {
+  font-family: "DM Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 700;
+  font-style: normal;
+}
+
+.dm-sans-semibold {
+  font-family: "DM Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 600;
+  font-style: normal;
+}
+
+.dm-sans-medium {
+  font-family: "DM Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 500;
+  font-style: normal;
+}
+
+.dm-sans-regular {
+  font-family: "DM Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,118 @@
+<script setup>
+  import { ArrowRightIcon } from '@heroicons/vue/24/solid'
+</script>
+
+<script>
+</script>
+
+<template>
+  <!--Main-->
+  <div class="flex flex-col justify-center items-center w-vw-full min-h-screen bg-gray-950">
+    <!--Header-->
+    <div class="flex justify-between items-center w-full py-4 dm-sans-medium gap-3 mx-auto max-w-7xl fixed top-0 z-50">
+
+      <!--Inline title-->
+      <div class="flex justify-center items-center">
+        <h1 class="text-white text-xl dm-sans-bold">SP Audio</h1>
+      </div>
+      <!--Inline title-->
+
+      <!--Nav-->
+      <div class="flex justify-center items-center rounded-3xl h-full ring-gray-800 ring-1 px-2 py-1">
+        <div class="px-4 py-1.5">
+          <!--Active test-->
+          <p class="text-sm text-white cursor-pointer border-b-2 border-white transition-opacity">Részletek</p>
+        </div>
+
+        <div class="px-4 py-1.5">
+          <p class="text-sm text-gray-400 cursor-pointer">Árajánlat</p>
+        </div>
+
+        <div class="px-4 py-1.5">
+          <p class="text-sm text-gray-400 cursor-pointer">Vélemények</p>
+        </div>
+
+        <div class="px-4 py-1.5">
+          <p class="text-sm text-gray-400 cursor-pointer">FAQ</p>
+        </div>
+      </div>
+      <!--Nav-->
+
+      <!--Inline title-->
+      <div class="flex justify-center items-center">
+        <h1 class="text-gray-950 text-xl dm-sans-bold">SP Audio</h1>
+      </div>
+      <!--Inline title-->
+
+    </div>
+    <!--Header-->
+
+    <!--Title body-->
+    <div class="flex flex-col justify-center items-center py-32 w-full h-full">
+      <div class="mt-12 w-full h-full"></div>
+      <h1 class="flex justify-center items-center text-center text-7xl dm-sans-bold text-white w-full h-full">
+        SP Audio
+      </h1>
+      <p class="flex justify-center items-center text-center text-lg dm-sans-regular text-gray-300 mt-6">
+        Profi DJ, modern hang- és fénytechnika áll rendelkezésre esküvőkhöz, céges rendezvényekhez és bulikhoz is.
+      </p>
+      <div class="mt-6 flex justify-center items-center w-full h-full">
+        <button class="rounded-full flex justify-center items-center border-none dm-sans-medium bg-white px-6 py-3">
+          Árajánlat
+
+          <ArrowRightIcon class="ml-2 h-6 w-6 text-xl" />
+        </button>
+      </div>
+    </div>
+    <!--Title body-->
+
+    <!--Promo image-->
+    <div>
+
+    </div>
+    <!--Promo image-->
+
+    <!--Promo cards-->
+    <div>
+
+    </div>
+    <!--Promo cards-->
+
+    <!--Social media-->
+    <div>
+
+    </div>
+    <!--Social media-->
+
+    <!--Price offer-->
+    <div>
+
+    </div>
+    <!--Price offer-->
+
+    <!--Reviews-->
+    <div>
+
+    </div>
+    <!--Reviews-->
+
+    <!--Stuff-->
+    <div>
+
+    </div>
+    <!--Stuff-->
+
+    <!--FAQ-->
+    <div>
+
+    </div>
+    <!--FAQ-->
+
+    <!--Footer-->
+    <div>
+
+    </div>
+    <!--Footer-->
+  </div>
+  <!--Main-->
+</template>
